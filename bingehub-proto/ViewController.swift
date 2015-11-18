601c9d57cb889cf1b5a142a4a564a1afa33720cc
Randy Burgess <wrburgess@gmail.com>
2015-11-17 21:11:16 -0600

    Initial Commit

---
+++ bingehub-proto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bingehub-proto
+//
+//  Created by Randy Burgess on 11/17/15.
+//  Copyright © 2015 All Aboard Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
